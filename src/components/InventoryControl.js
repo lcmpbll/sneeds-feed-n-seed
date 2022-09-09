87e22680d0f784711b539ed243970faa475d289b
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-09 16:12:11 -0700

    Add selling property

--- src/components/InventoryControl.js
+++ src/components/InventoryControl.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainInventoryList from './MainInventoryList'
 import FeedDetail from './FeedDetail';
 import NewFeedForm from './NewFeedForm';
+import SellFeedForm from './SellFeedForm';
 
 class InventoryControl extends React.Component {
   constructor(props) {
@@ -9,11 +10,12 @@ class InventoryControl extends React.Component {
     this.state = {
       formVisibleOnPage : false,
       selectedFeed: null,
-      mainInventoryList : []
+      mainInventoryList : [],
+      selling: false,
     }
     this.handleShowForm = this.handleShowForm.bind(this);
   }
-  //toggles?
+ 
   handleShowForm = () => {
     if(this.state.selectedFeed === null) { 
       this.setState((prevState) => ({
@@ -24,14 +26,30 @@ class InventoryControl extends React.Component {
       this.setState({
         formVisibleOnPage: false,
         selectedFeed: null,
+        selling: false
       });
      
     }
   }
   
+  
   handleAddingFeedToList = (newFeed) => {
     const newMainInventoryList = this.state.mainInventoryList.concat(newFeed);
     this.setState({ mainInventoryList: newMainInventoryList});
+  }
+  
+  handleSellClick = () => {
+    
+    this.setState({selling: true});
+  }
+  
+  handleSoldFeedInList = (feedToSell) => {
+    const updatedMainInventoryList = this.state.mainInventoryList.filter(feed => feed.id !== this.state.selectedFeed.id).concat(feedToSell);
+    this.setState({
+      mainInventoryList: updatedMainInventoryList,
+      selling: false,
+      selectedFeed: null
+    });
   }
   
   handleChangingSelectedFeed = (id) => {
@@ -43,9 +61,11 @@ class InventoryControl extends React.Component {
   render() {
     let currentlyVisibleState = false;
     let buttonText = null;
-    
-    if(this.state.selectedFeed != null){
-      currentlyVisibleState = <FeedDetail feed= {this.state.selectedFeed} />
+    if(this.state.selling){
+      currentlyVisibleState = <SellFeedForm feed={this.state.selectedFeed} onSellFeed={this.handleSoldFeedInList} />
+      buttonText = "Return to Inventory";  
+    } else if(this.state.selectedFeed != null){
+      currentlyVisibleState = <FeedDetail feed= {this.state.selectedFeed} onClickingSell= {this.handleSellClick} />
       buttonText = 'Return to main inventory';
     } else if(this.state.formVisibleOnPage) {
       currentlyVisibleState = (
