2646a29fbcd4aee000b0849ffd8fbc4a986bb4a4
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-09 16:11:58 -0700

    Update html property labels

--- src/components/FeedDetail.js
+++ src/components/FeedDetail.js
@@ -7,10 +7,10 @@ function FeedDetail(props) {
   return [
     <React.Fragment  key={feed.id}>
       <h1>{feed.name} Details</h1>
-      <h2>Total bags: {feed.quantity}</h2>
-      <h4>Current bag remaining: {feed.bagPounds}</h4>
+      <h2>Unopened bags: {feed.quantity}</h2>
+      <h4>Open bag weight: {feed.bagPounds}</h4>
       <h4>Price per lb: {feed.price}</h4>
-     
+      <button onClick= {props.onClickingSell}>Sell {feed.name}</button>
       <hr />
     </React.Fragment>
   ]
@@ -18,6 +18,7 @@ function FeedDetail(props) {
 
 FeedDetail.propTypes = {
   feed: PropTypes.object,
+  onClickingSell: PropTypes.func
 };
 
 export default FeedDetail;
