31501e169b882baee7d54e536223251edbf51e1e
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-09 13:29:47 -0700

    Add feed id to react fragment

--- src/components/FeedDetail.js
+++ src/components/FeedDetail.js
@@ -5,12 +5,12 @@ function FeedDetail(props) {
   const { feed } = props;
   
   return [
-    <React.Fragment>
+    <React.Fragment  key={feed.id}>
       <h1>{feed.name} Details</h1>
       <h2>Total bags: {feed.quantity}</h2>
-      <h4>Current bag remaining: {feed.pounds}</h4>
+      <h4>Current bag remaining: {feed.bagPounds}</h4>
       <h4>Price per lb: {feed.price}</h4>
-      {feed.id}
+     
       <hr />
     </React.Fragment>
   ]
