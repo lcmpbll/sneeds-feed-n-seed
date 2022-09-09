0525218d47defecb57c382a00a32e2c8e2f866e2
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-09 11:45:52 -0700

    Remove pounds from form

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReusableForm(props){
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <div className='mb-3'>
+          <label htmlFor='name' className='form-label'>Feed or Seed Name:</label>
+          <input type='text' className='form-control' name='name' />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='quantity' className='form-label'>Quantity of bags added</label>
+          <input type='number' className='form-control' name='quantity' />
+        </div>
+        <div className='mb-3'>  
+          <label htmlFor='price' className='form-label'>Price per lb:</label>
+          <input type='number' className='form-control' name='price' />
+        </div>  
+        <button type='submit' className='btn btn-success'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+export default ReusableForm;
