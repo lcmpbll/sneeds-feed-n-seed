e87446beb2e2ec292f6a9ac078fad3502fe3c5ce
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-07 19:43:28 -0700

    Add classes, display rows, in column

--- src/components/FeedDetail.js
+++ src/components/FeedDetail.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function FeedDetail(props) {
-  const { feed } = props;
-  
-  return [
-    <React.Fragment  key={feed.id}>
+  const { feed, onClickingSell} = props;
+  console.log(props);
+  return (
+    <div  className='sneeds__feed' key={feed.id}>
+      <div className='sneeds__feed-card'>
       <h1>{feed.name} Details</h1>
       <h2>Unopened bags: {feed.quantity}</h2>
       <h4>Open bag weight: {feed.bagPounds}</h4>
       <h4>Price per lb: {feed.price}</h4>
-      <button onClick= {props.onClickingSell}>Sell {feed.name}</button>
-      <hr />
-    </React.Fragment>
-  ]
+      <button onClick= {onClickingSell}>Sell {feed.name}</button>
+      </div>
+    </div>
+  );
 }
 
 FeedDetail.propTypes = {
